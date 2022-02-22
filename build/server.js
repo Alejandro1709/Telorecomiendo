e868caef22a45ff3b4338a07b421d5d02553f0f8
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-02-22 18:14:07 -0500

    add basic endpints

--- build/server.js
+++ build/server.js
@@ -9,7 +9,13 @@ var path_1 = __importDefault(require("path"));
 var app = (0, express_1.default)();
 var hotels = JSON.parse(fs_1.default.readFileSync(path_1.default.join(__dirname, '../data/hotels.json'), 'utf8'));
 app.get('/', function (req, res) {
-    console.table(hotels);
     res.send(hotels);
+});
+app.get('/:slug', function (req, res) {
+    var hotel = hotels.find(function (h) { return h.hotelSlug === req.params.slug; });
+    if (!hotel) {
+        return res.status(404).json({ message: 'Hotel Not Found!' });
+    }
+    res.send(hotel);
 });
 app.listen(2000, function () { return console.log('Server is live at: http://localhost:2000'); });
--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,22 @@ import path from 'path';
 
 const app: Application = express();
 
-const hotels = JSON.parse(
+const hotels: any[] = JSON.parse(
   fs.readFileSync(path.join(__dirname, '../data/hotels.json'), 'utf8')
 );
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response): void => {
   res.send(hotels);
+});
+
+app.get('/:slug', (req: Request, res: Response) => {
+  let hotel = hotels.find((h) => h.hotelSlug === req.params.slug);
+
+  if (!hotel) {
+    return res.status(404).json({ message: 'Hotel Not Found!' });
+  }
+
+  res.send(hotel);
 });
 
 app.listen(2000, () => console.log('Server is live at: http://localhost:2000'));
