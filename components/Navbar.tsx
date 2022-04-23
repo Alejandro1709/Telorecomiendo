7301d319c756cdb07d03e36b5adedcc6323eb12c
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-04-23 16:26:40 -0500

    separated Header, Navbar into its own components

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Navbar() {
+	return (
+		<nav
+			className='flex flex-col gap-4 md:flex-row md:gap-0 justify-between items-center'
+			role='navigation'
+		>
+			<h1 className='text-3xl font-semibold text-white select-none'>
+            hotelsnow.com
+			</h1>
+			<ul className='flex align-baseline md:gap-4 gap-2'>
+				<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
+                Home
+				</li>
+				<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
+                Hotels
+				</li>
+				<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
+                Countries
+				</li>
+				<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
+                Account
+				</li>
+			</ul>
+		</nav>
+	)
+}
+
+export default Navbar
