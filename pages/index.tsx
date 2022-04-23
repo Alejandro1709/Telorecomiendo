46faacce99ef1f94fcb55a6122a19915ae9cd3e1
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-04-23 13:48:27 -0500

    fixed issues with lint

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,24 +14,24 @@ const Home: NextPage = () => {
 					role='navigation'
 				>
 					<h1 className='text-3xl font-semibold text-white select-none'>
-            hotelsnow.com
+						hotelsnow.com
 					</h1>
 					<ul className='flex align-baseline md:gap-4 gap-2'>
 						<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
-              Home
+							Home
 						</li>
 						<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
-              Hotels
+							Hotels
 						</li>
 						<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
-              Countries
+							Countries
 						</li>
 						<li className='text-white font-semibold text-lg cursor-pointer hover:text-black'>
-              Account
+							Account
 						</li>
 					</ul>
 				</nav>
-        fkfk
+				fkfk
 			</header>
 		</Layout>
 	)
