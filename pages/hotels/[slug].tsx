cdbdddd578093984671f26b523bae5a4e9c39868
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-04-26 17:05:44 -0500

    add hotel header and main actions

--- pages/hotels/[slug].tsx
+++ pages/hotels/[slug].tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import Header from '../../components/Header'
@@ -16,6 +16,11 @@ function HotelPage() {
 
 	const [currentHotel, setCurrentHotel] = useState<Hotel | undefined>(hotel)
 
+	useEffect(() => {
+		setCurrentHotel(hotel)
+	}, [])
+	
+
 	return (
 		<Layout title={currentHotel !== undefined ? currentHotel.name : 'Hotel Name'} description={currentHotel !== undefined ? currentHotel.desc : 'Hotel Description'}>
 			<Header>
@@ -23,15 +28,30 @@ function HotelPage() {
 					<span className='p-2 bg-white rounded-md hover:bg-gray-200'>
 						<Link href='/'>Go back</Link>
 					</span>
-					<h2 className='text-2xl text-center font-semibold text-white'>{currentHotel !== undefined ? currentHotel.name : 'Hotel Name'}</h2>
+					<h2 className='md:text-2xl text-center font-semibold text-white'>{currentHotel !== undefined ? currentHotel.name : 'Hotel Name'}</h2>
 					<span className='p-2 bg-white rounded-md hover:bg-gray-200'>
 						<Link href='/'>Share</Link>
 					</span>
 				</div>
 			</Header>
-			<div>
-				fkfk
-			</div>
+			<section className='flex flex-col md:flex-row-reverse m-8 gap-8'>
+				{/* Image */}
+				<div className='bg-gray-100 w-full h-64' />
+				<div className='w-full h-64'>
+					<ul className='text-lg'>
+						<li><strong>Name:</strong> {currentHotel?.name}.</li>
+						<li><strong>Address:</strong> {currentHotel?.address}.</li>
+						<li><strong>Price:</strong> {currentHotel?.price}USD/Night.</li>
+					</ul>
+					{/* Gallery */}
+					<div className='flex flex-wrap md:grid md:grid-cols-4 sm:grid-cols-2 gap-2 mt-4'>
+						<div className='h-[150px] w-[150px] bg-red-200'></div>
+						<div className='h-[150px] w-[150px] bg-red-200'></div>
+						<div className='h-[150px] w-[150px] bg-red-200'></div>
+						<div className='h-[150px] w-[150px] bg-red-200'></div>
+					</div>
+				</div>
+			</section>
 		</Layout>
 	)
 }
