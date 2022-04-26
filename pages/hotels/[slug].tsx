f3aaea0c83e33955d161d2959489380345f719f1
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-04-26 13:11:28 -0500

    add hotel page

--- pages/hotels/[slug].tsx
+++ pages/hotels/[slug].tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Header from '../../components/Header'
+import Layout from '../../components/Layout'
 
 function HotelPage() {
 	return (
-		<div>HotelPage</div>
+		<Layout title='Hotel Page' description=''>
+			<Header/>
+			<div>
+				fkfk
+			</div>
+		</Layout>
 	)
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const HomePage: NextPage = () => {
 			<Header>
 				<HotelList />
 			</Header>
-			<Showcase />
+			{/* <Showcase /> */}
 		</Layout>
 	)
 }
