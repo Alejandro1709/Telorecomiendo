02a177b3528054482567b8a4d324e8722fbab28d
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-05-07 18:20:23 -0500

    add reviews section

--- pages/hotels/[slug].tsx
+++ pages/hotels/[slug].tsx
@@ -37,7 +37,7 @@ function HotelPage() {
 			<section className='flex flex-col md:flex-row-reverse m-8 gap-8'>
 				{/* Image */}
 				<div className='bg-gray-100 w-full h-64' />
-				<div className='w-full h-64'>
+				<div className='w-full'>
 					<ul className='text-lg'>
 						<li><strong>Name:</strong> {currentHotel?.name}.</li>
 						<li><strong>Address:</strong> {currentHotel?.address}.</li>
@@ -45,11 +45,20 @@ function HotelPage() {
 					</ul>
 					{/* Gallery */}
 					<div className='flex flex-wrap md:grid md:grid-cols-4 sm:grid-cols-2 gap-2 mt-4'>
-						<div className='h-[150px] w-[150px] bg-red-200'></div>
-						<div className='h-[150px] w-[150px] bg-red-200'></div>
-						<div className='h-[150px] w-[150px] bg-red-200'></div>
-						<div className='h-[150px] w-[150px] bg-red-200'></div>
+						<div className='h-[150px] w-[150px] bg-red-200 rounded-md'></div>
+						<div className='h-[150px] w-[150px] bg-red-200 rounded-md'></div>
+						<div className='h-[150px] w-[150px] bg-red-200 rounded-md'></div>
+						<div className='h-[150px] w-[150px] bg-red-200 rounded-md'></div>
 					</div>
+				</div>
+			</section>
+			<section className='p-8 border-t border-b'>
+				<h2 className='text-3xl font-semibold mb-8'>See some reviews</h2>
+				<div className='flex flex-row gap-2 overflow-x-scroll w-full shrink-0 flex-0'>
+					<div className='bg-gray-200 w-[260px] h-24 rounded-md'>fkk</div>
+					<div className='bg-gray-200 w-[260px] h-24 rounded-md'>fkk</div>
+					<div className='bg-gray-200 w-[260px] h-24 rounded-md'>fkk</div>
+					<div className='bg-gray-200 w-[260px] h-24 rounded-md'>fkk</div>
 				</div>
 			</section>
 		</Layout>
